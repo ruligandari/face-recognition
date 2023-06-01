1b74e34dfaedda919bf225f54d590d6edf5e4434
ruligandari <ruligandari@gmail.com>
2023-06-01 19:47:07 +0700

    fix tampilan kamera webcam

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ const video = document.getElementById("video");
 
 Promise.all([
   faceapi.nets.ssdMobilenetv1.loadFromUri("/models"),
-  faceapi.nets.tinyFaceDetector.loadFromUri("/models"),
   faceapi.nets.faceRecognitionNet.loadFromUri("/models"),
   faceapi.nets.faceLandmark68Net.loadFromUri("/models"),
 ]).then(startWebcam);
